0c8a4062e168d497531a5bb6dd063433be661670
Erik Xu <81636759@qq.com>
2023-01-28 02:26:26 +0800

    Add port to code server start.

--- cmd/code_server/code_server_start.go
+++ cmd/code_server/code_server_start.go
@@ -3,16 +3,23 @@ package code_server
 import (
 	"fmt"
 	"power-ci/utils"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-var startScript = `#!/bin/bash
+func init() {
+	codeServerStartCmd.Flags().StringVarP(&PORT, "port", "p", "80", "Bind port")
+}
+
+var PORT string
+
+var template = `#!/bin/bash
 PASSWORD=$(uuidgen)
 
 mkdir -p ~/.config/code-server
 cat>~/.config/code-server/config.yaml<<EOF
-bind-addr: 0.0.0.0:80
+bind-addr: 0.0.0.0:{PORT}
 auth: password
 password: ${PASSWORD}
 cert: false
@@ -25,7 +32,8 @@ var codeServerStartCmd = &cobra.Command{
 	Use:   "start",
 	Short: "Start code server",
 	Run: func(cmd *cobra.Command, args []string) {
-		filepath := utils.WriteScript("start-code-server.sh", startScript)
+		script := strings.Replace(template, "{PORT}", PORT, -1)
+		filepath := utils.WriteScript("start-code-server.sh", script)
 		utils.ExecuteScript(filepath)
 		fmt.Println("Start success")
 	},
